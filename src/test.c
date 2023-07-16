f6b9aebce16580208312c76b7d2695cedbd4e1ff
Oskar <Oskar>
2023-07-16 23:05:20 +0200

    Deleting unnecessary #define directive

--- src/test.c
+++ src/test.c
@@ -4,15 +4,14 @@
 #include <string.h>
 #include <assert.h>
 
-#define SOMETHING
 #include "../API/testing_directive.h"
 
 int main(void){
     printf("Value of the output: %s\n", testOutput());
 
     assert(strcmp(testOutput(), "Linux statement")); /*TEST: if you are using linux */
-    
-    /*assert(!strcmp(testOutput(), "Windows statement")); TEST: if you are using windows */
+
+    /*assert(strcmp(testOutput(), "Windows statement")); TEST: if you are using windows */
 
     return EXIT_SUCCESS;
 }
